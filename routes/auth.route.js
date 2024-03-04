0b0b8cad98159c7533fa18cc10ff7fee021e0702
DevaNanda <nandadeva870@gmail.com>
2024-03-04 23:49:43 +0700

    inital commit

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { register, login } = require('../controller/auth.controller');
+const { register, login } = require('../controllers/auth.controller');
 
 const route = express.Router();
 
